a1c4bcac5df7ff60f4e0c53e816a58efb2b0659c
Oriol Puig <opuig@pasiona.com>
2019-05-19 15:41:00 +0200

    Initial commit

---
+++ UsersManagement.UI/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace UsersManagement.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ UsersManagement.UI/Models/Account/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersManagement.UI.Models.Account
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string RedirectUrl { get; set; }
+    }
+}
