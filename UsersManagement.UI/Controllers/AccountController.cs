672e4fdf24820c31bc6d743a821e894e232d43d5
Oriol Puig <opuig@pasiona.com>
2019-05-19 15:43:25 +0200

    Add Login post method

--- UsersManagement.UI/Controllers/AccountController.cs
+++ UsersManagement.UI/Controllers/AccountController.cs
@@ -1,13 +1,31 @@
 ﻿using System.Web.Mvc;
+using UsersManagement.UI.Models.Account;
 
 namespace UsersManagement.UI.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Login()
+        public ActionResult Login(string returnUrl)
         {
-            return View();
+            return View(new LoginViewModel { RedirectUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                return Redirect(Url.Content(returnUrl));
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
