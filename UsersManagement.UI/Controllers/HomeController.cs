9a174a45b305d072f230ea7f26e43245edf74111
Oriol Puig <opuig@pasiona.com>
2019-06-04 19:56:01 +0200

    Create and secure a page per each role

--- UsersManagement.UI/Controllers/HomeController.cs
+++ UsersManagement.UI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using UsersManagement.UI.Models.Extensions;
 
 namespace UsersManagement.UI.Controllers
 {
@@ -7,6 +8,35 @@ namespace UsersManagement.UI.Controllers
         // GET: Home
         [Authorize]
         public ActionResult Index()
+        {
+            if (User.IsAdmin()) return RedirectToAction("Admin");
+            else if (User.IsPage1()) return RedirectToAction("Page_1");
+            else if (User.IsPage2()) return RedirectToAction("Page_2");
+            else if (User.IsPage3()) return RedirectToAction("Page_3");
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "PAGE_1")]
+        public ActionResult Page_1()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "PAGE_2")]
+        public ActionResult Page_2()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "PAGE_3")]
+        public ActionResult Page_3()
         {
             return View();
         }
